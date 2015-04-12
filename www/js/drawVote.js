6f0b1e7576327664c85f00b469549b114867c5bd
yiskang <m9805508@mail.ntust.edu.tw>
2015-04-12 08:16:34 +0800

    Change folder WWW to www

---
+++ www/js/drawVote.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+	var canvas = document.getElementById('goodArea');
+	var context = canvas.getContext('2d');
+
+
+	
+	context.beginPath();
+	context.rect( 0, 0, canvas.width, canvas.height);
+	context.fillStyle = 'lightblue';
+	context.fill();
+
+	context.font = 'bold 100pt Calibri';
+	context.fillStyle = 'white';
+	context.textAlign = 'right';
+    context.fillText('+1', canvas.width, canvas.height);
+
+
+	var neutralArea = document.getElementById('neutralArea');
+	var context = neutralArea.getContext('2d');
+
+	context.beginPath();
+	context.rect( 0, 0, canvas.width, canvas.height);
+	context.fillStyle = '#888888';
+	context.fill();
+
+
+	var canvas = document.getElementById('badArea');
+	var context = canvas.getContext('2d');
+
+	context.beginPath();
+	context.rect( 0, 0, canvas.width, canvas.height);
+	context.fillStyle = 'pink';
+	context.fill();	
+
+	context.font = 'bold 100pt Calibri';
+	context.fillStyle = 'white';
+	context.textAlign = 'right';
+    context.fillText('-1', canvas.width, canvas.height);
+
+});
+
+function drawCandidate(id, name, img) {
+	var c = document.getElementById(id);
+	var ctx = c.getContext("2d");
+	
+	ctx.beginPath();
+	ctx.arc(50 ,50, 45, 0, 2*Math.PI);
+	ctx.lineWidth = 5;
+	ctx.stroke();
+
+	var thumbnail = new Image();
+	thumbnail.src = img;
+	thumbnail.width = c.width;
+	thumbnail.height = c.height; 
+	thumbnail.onload = function(){
+		var pattern = ctx.createPattern(this, "repeat");
+		ctx.fillStyle = pattern;
+		ctx.fill();
+	};
+	
+	ctx.font = "20px";
+	ctx.textAlign = "center";
+	ctx.fillText(name, 50, 108);
+}
